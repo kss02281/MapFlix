4686a14952daf827140b13e0fa55c3cadc64f906
minji <my970524@naver.com>
2021-10-08 16:15:21 +0900

    timeline api

--- backend/config.py
+++ backend/config.py
@@ -2,6 +2,6 @@ import os
 
 BASE_DIR = os.path.dirname(__file__)
 
-SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/test'
+SQLALCHEMY_DATABASE_URI = 'mysql+pymysql://root:root@localhost:3306/covid_mapflix'
 # uri = mysql+pymysql://mysql_id/mysql_pw/localhost:3306/사용할 db 이름
 SQLALCHEMY_TRACK_MODIFICATIONS = False
