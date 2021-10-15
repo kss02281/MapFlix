d0dbfe66847c742abee73fafca57df3d563c0ece
[2기-1반] 김재현 <jaehy0429@gmail.com>
2021-10-15 17:24:17 +0900

    add hover arrow

--- frontend/src/Components/MapHoverYellow.jsx
+++ frontend/src/Components/MapHoverYellow.jsx
@@ -3,38 +3,12 @@ import { BarChart, Bar, XAxis, YAxis, CartesianGrid } from "recharts";
 import "../css/MapHoverYellow.scss";
 
 function MapHoverYellow(props) {
-  const [subscibesQ1, setSubscibesQ1] = useState(0);
-  const [subscibesQ2, setSubscibesQ2] = useState(0);
   const [movieURL, setMovieURL] = useState("");
   const [showURL, setShowURL] = useState("");
   const [movieTitle, setMovieTitle] = useState("");
   const [showTitle, setShowTitle] = useState("");
   const nationName = props.nationName;
   const nationCode = props.nationCode;
-
-  const subscibes_data = [
-    {
-      name: "2021 Q1",
-      pv: subscibesQ1 / 100,
-    },
-    {
-      name: "2021 Q2",
-      pv: subscibesQ2 / 100,
-    },
-  ];
-  useMemo(() => {
-    console.log("/netflix/" + nationCode);
-    fetch("/netflix/" + nationCode)
-      .then((response) => {
-        if (response.ok) {
-          return response.json();
-        }
-      })
-      .then((data) => {
-        setSubscibesQ1(data.q1_subscribers);
-        setSubscibesQ2(data.q2_subscribers);
-      });
-  });
 
   useMemo(() => {
     console.log("/netflix/" + nationCode + "/2021-040/top1");
@@ -53,13 +27,6 @@ function MapHoverYellow(props) {
         setShowTitle(show.title);
       });
   });
-  const increase_Subscibes_Data = subscibesQ2 - subscibesQ1;
-  const increase_Color =
-    increase_Subscibes_Data > 0
-      ? "red"
-      : increase_Subscibes_Data == 0
-      ? "black"
-      : "blue";
   return (
     <div className="MapHoverYellow_Wrap">
       <div className="countryName">{nationName}</div>
