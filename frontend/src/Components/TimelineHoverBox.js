360d4db15c29aa5f40ae72770eaa23b62ef84fa5
[2기-1반]이선예 <cci3737@naver.com>
2021-10-15 16:23:04 +0900

    completeTimelineHover

--- frontend/src/Components/TimelineHoverBox.js
+++ frontend/src/Components/TimelineHoverBox.js
@@ -1,56 +1,37 @@
 import React, { useEffect, useMemo, useState } from 'react';
-import { shallowEqual, useSelector } from "react-redux";
+import Title from './Title';
 import '../css/TimelineHoverBox.scss'
 
 function TimelineHoverBox(props) {
 
-    const nationName = props.nation;
-    const nationCode = props.nationCode;
     const fullWeek = props.fullWeek;
     const confirmedCnt = props.confirmedCnt;
-    const topList = props.content;
-    const [top1List, setTop1List] = useState('');
+    const movie = props.movie;
+    const show = props.show;
 
-
-    const contentList = useSelector(
-        state => (
-            state.topContentList.topContent[fullWeek]
-        ),
-        shallowEqual
-    )
-
-    
     const [year, week] = [parseInt(fullWeek.slice(0,4)), parseInt(fullWeek.slice(5,8))];
-
-
-
-    useEffect(() => {
-        if (year === 2020 && week < 33) {
-            
-        }else if (year === 2021 && week === 1 && nationCode != 'kr'){
-            
-        }else {
-            if(topList)
-            { 
-                console.log('topList', topList?.index(0))
-                setTop1List(top1List[0]?.title)
-            }
-        }
-    },[ fullWeek, confirmedCnt ])
 
     return (
         <div className="hoverContainerT">
-            <div>{year}, WEEK {week}</div>
-            <div>New Confirmed : {confirmedCnt}</div>
-            <hr></hr>
+            <div className='hoverTimeTop'>
+                <div>{year}, WEEK {week}</div>
+                <div>New Confirmed : {confirmedCnt}</div>
+            </div>
+            
+            <hr/>
             <div className="top1ContentT">
                 <div className="movieT">
-                    MOVIE {top1List}
-                    
+                    <div className='hoverTimeType'>TOP MOVIE</div>
+                    <img className='hoverImg' src={movie?.poster} />
+                    <br/>
+                    <div className='hoverTimeTitle'>{movie?.title}</div>
                 </div>
+                <div className="blank"></div>
                 <div className="tvshowsT">
-                    TVSHOWS
-                    {top1List}
+                    <div className='hoverTimeType'>TOP TVSHOWS</div>
+                    <img className='hoverImg' src={show?.poster} />
+                    <br/>
+                    <div className='hoverTimeTitle'>{show?.title}</div>
                 </div>
             </div>
         </div>
