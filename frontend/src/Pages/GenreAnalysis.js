ed2ff96d94df1fc6b2c5835fc09b512193491fb4
[2기-1반]이선예 <cci3737@naver.com>
2021-10-15 17:24:02 +0900

    useSelector_genreScore

--- frontend/src/Pages/GenreAnalysis.js
+++ frontend/src/Pages/GenreAnalysis.js
@@ -2,21 +2,27 @@ import React, { useMemo }  from "react";
 import '../css/GenreAnalysis.scss';
 import { useLocation, useHistory } from "react-router-dom";
 import { GenreTimeLine } from ".";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector, shallowEqual } from "react-redux";
 import queryString from "query-string";
 import { getGenreScore } from "../Redux/actions/genreScore";
 
 function GenreAnalysis({history, location}) {
   const query = queryString.parse(location.search);
-  console.log(query);
+
   const { nation, nationCode } = query;
   const dispatch = useDispatch();
+  const genreScore = useSelector(
+    (state) => state.genreScore.genreScore,
+    shallowEqual
+  )
 
   useMemo(()=>{
     dispatch(getGenreScore({
       nationCode: nationCode,
       week: '2021-039',
     }))
+    
+    console.log(genreScore)
   },[])
 
   return (
